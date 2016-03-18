19c991f31cf9638bcf9f879d4e38885ff4be1b7a
a.zalonis <a.zalonis@sigmasolutions.gr>
2016-03-18 13:59:57 +0200

    initial commit

---
+++ RealFMWApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RealFMWApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Transparent;
+            this.Play.FlatAppearance.BorderSize = 0;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Play.Image = global::RealFMWApp.Properties.Resources.play;
+            this.Play.Location = new System.Drawing.Point(138, 117);
+            this.Play.Margin = new System.Windows.Forms.Padding(0);
+            this.Play.Name = "Play";
+            this.Play.Padding = new System.Windows.Forms.Padding(4);
+            this.Play.Size = new System.Drawing.Size(179, 115);
+            this.Play.TabIndex = 0;
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RealFMWApp.Properties.Resources.realfm;
+            this.ClientSize = new System.Drawing.Size(475, 244);
+            this.Controls.Add(this.Play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RealFM";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveBorder;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play;
+    }
+}
+
